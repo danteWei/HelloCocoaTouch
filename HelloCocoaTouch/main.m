883095041fd02362f3027c20a1e74c0630eb2eff
Dante <user@UsermatoMacBook-Pro.local>
2013-01-31 00:16:52 -0400

    Initial Commit

---
+++ HelloCocoaTouch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloCocoaTouch
+//
+//  Created by Dante on 13-1-31.
+//  Copyright (c) 2013年 Dante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
